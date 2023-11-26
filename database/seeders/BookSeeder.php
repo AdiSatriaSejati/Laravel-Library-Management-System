25d42120f82cb42beda14978c8c29ea28b107852
Adi Satria Sejati <117582840+AdiSatriaSejati@users.noreply.github.com>
2023-11-26 15:41:39 +0700

    Add files via upload

---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Book;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Book::factory()->count(50)->create();
+    }
+}
